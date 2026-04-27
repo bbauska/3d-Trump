8a71695a2723f44794018be10e79a1b61462aefd
Brian Bauska <brianbauska@gmail.com>
2026-04-26 18:22:56 -0700

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,30 @@
+/* 3d-Trump/js/script.js for readout.bauska.org */
+// Place text "statement of Trump lies" into const readOut.
+const readOut = `<p>
+ <span>subscribers</span>
+</p>`;
+
+// Function to insert silusGW into divs
+function insertreadOutIntoDivs() {
+  // Get all .text divs
+  const textDivs = document.querySelectorAll(".text");
+
+  // Insert readOut into all .text divs.
+  textDivs.forEach((div) => {
+    div.innerHTML = readOut;
+  });
+}
+
+// Call the function when the DOM is fully loaded
+document.addEventListener("DOMContentLoaded", insertreadOutIntoDivs);
+
+const contentDiv = document.querySelector(".content");
+function adjustContentSize() {
+  const viewportWidth = window.innerWidth;
+  const baseWidth = 1000;
+  const scaleFactor =
+    viewportWidth < baseWidth ? (viewportWidth / baseWidth) * 0.8 : 1;
+  contentDiv.style.transform = `scale(${scaleFactor})`;
+}
+window.addEventListener("load", adjustContentSize);
+window.addEventListener("resize", adjustContentSize);
